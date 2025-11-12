08d2863e24ea3778ed85d65c14b6f30e0fe4be4b
Ivan <vandudkin@gmail.com>
2025-11-12 20:09:12 +0100

    Something

---
+++ Abgabe_7/src/gameoflife/Cell.java
@@ -0,0 +1,45 @@
+package gameoflife;
+
+public class Cell {
+    private boolean alive;
+    private int livingNeighbors;
+    Cell[] neighbors;
+
+    public Cell(double probabilityToLive) {
+        this.alive = Math.random() < probabilityToLive;
+        this.neighbors = new Cell[8];
+    }
+
+    public void addNeighbor(Cell cell) {
+        for (int cellIndex = 0; cellIndex < this.neighbors.length; cellIndex++) {
+            if (this.neighbors[cellIndex] == null) {
+                this.neighbors[cellIndex] = cell;
+                break;
+            }
+        }
+    }
+
+    void countLivingNeighbors() {
+        int counter = 0;
+        for (Cell neighbor : this.neighbors) {
+            if (neighbor.alive) {
+                counter++;
+            }
+        }
+        this.livingNeighbors = counter;
+    }
+
+    public void applyRules() {
+        if (this.livingNeighbors < 2 || this.livingNeighbors > 3) {
+            this.alive = false;
+        }
+        if (this.livingNeighbors == 3) {
+            this.alive = true;
+        }
+    }
+
+    public String asString() {
+        return this.alive ? "X" : " ";
+    }
+
+}
