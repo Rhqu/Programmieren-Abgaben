47c7da35853ae123d4b361dd57735efe1cc1bcf7
Rhqu <vandudkin@gmail.com>
2025-11-20 00:31:27 +0100

    Update

--- Abgabe_5/src/methods/Methods.java
+++ Abgabe_5/src/methods/Methods.java
@@ -1,17 +1,50 @@
 package methods;
 
 public class Methods {
-    static void even(int i){
-        if(i % 2 == 0){
-            System.out.printf("Zahl %d ist gerade", i);
-        } else{
-            System.out.printf("Zahl %d ist ungerade", i);
-        }
+    static boolean even(int i) {
+        return i % 2 == 0;
     }
+
+    static double pythagoras(int a, int b) {
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+
+    static int minimum(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+
+    static void printInfo() {
+        System.out.println("--- Abgabe 5 ---");
+    }
+
+    static void printVariable(String name, int value) {
+        System.out.println(name + value);
+    }
+
+    static void printRoundedDouble(String name, double roundedDouble) {
+        System.out.printf(name + "%.2f\n", roundedDouble);
+    }
+
+    static void printBoolean(String name, boolean bool) {
+        System.out.println(name + bool);
+    }
+
+    static void printInt(String name, int integer) {
+        System.out.println(name + integer);
+    }
+
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
         int c = 20;
-        even(a);
+        printInfo();
+        printVariable("int a: ", a);
+        printVariable("int b: ", b);
+        printVariable("int c: ", c);
+        printBoolean("even(a): ", even(a));
+        printRoundedDouble("pythagoras(a, b): ", pythagoras(a, b));
+        printInt("minimum(a, b, c): ", minimum(a, b, c));
+        printBoolean("Minimum von a, b und c ist ungerade: ", !even(minimum(a, b, c)));
+        printInfo();
     }
 }
--- Abgabe_7/src/gameoflife/PetriDish.java
+++ Abgabe_7/src/gameoflife/PetriDish.java
@@ -1,4 +1,32 @@
 package gameoflife;
 
+
+import javax.swing.*;
+
 public class PetriDish {
+    Cell[][] cells;
+    int lines;
+    int columns;
+    int generationNumber;
+
+    PetriDish(int columns, int lines, double probabilityToLive){
+        this.lines = lines;
+        this.columns = columns;
+        this.generationNumber = 1;
+        this.cells = new Cell[lines][columns];
+        createAllCells(probabilityToLive);
+    }
+    public void createAllCells(double probabilityToLive){
+        for (int i = 0; i < lines; i++){
+            for (int k = 0; k < columns; k++){
+                cells[i][k] = new Cell(probabilityToLive);
+            }
+        }
+    }
+    public void addNeighborsToCellIn(int line, int column){
+
+    }
+    public void addNeighborsToAllCells(){
+
+    }
 }
